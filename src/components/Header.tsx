6f8d8ab5558f8ca031914c3db89a8c03d0069b48
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-05-16 21:31:15 -0300

    feat: style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react"
-import { RiSearchLine } from "react-icons/ri"
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react"
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from "react-icons/ri"
 
 export default function Header() {
   return (
@@ -43,6 +43,36 @@ export default function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Leonardo Dantas</Text>
+            <Text color="gray.300" fontSize="small">
+              leo.vdn@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Leonado Dantas"
+            src="https://github.com/leovdn.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
