3e73d1e0bfe32235e8c8b4d56390be0fa68e849d
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-05-18 00:17:09 -0300

    feat: add Sidebar and dashboard graphics

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,62 @@
-import Header from "../components/Header"
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const options = {
+  chart: {
+    toolbar: false,
+    id: "basic-bar",
+    zoomEnabled: false,
+    foreColor: theme.colors.gray[500],
+  },
+  grid: { show: false },
+  dataLabels: { enabled: false },
+  tooltip: { enabled: false },
+  xaxis: {
+    type: "datetime",
+    axisBorder: { color: theme.colors.gray[600] },
+    axisTicks: { color: theme.colors.gray[600] },
+    categories: [
+      "2021-04-12T00:00:00.000Z",
+      "2021-04-13T00:00:00.000Z",
+      "2021-04-14T00:00:00.000Z",
+      "2021-04-15T00:00:00.000Z",
+      "2021-04-16T00:00:00.000Z",
+      "2021-04-17T00:00:00.000Z",
+      "2021-04-18T00:00:00.000Z",
+    ],
+  },
+};
+const series = [{ name: "series1", data: [31, 120, 10, 28, 61, 180, 109] }];
 
 export default function Dashboard() {
-  return <Header />
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex w="100%" my="6" maxWidth="1480" mx="auto" px="6">
+        <Sidebar />
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignItems="flex-start"
+        >
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Inscritos da Semana
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
+          </Box>
+          <Box p="8" bg="gray.800">
+            <Text fontSize="lg" mb="4">
+              Taxa de Abertura
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  );
 }
