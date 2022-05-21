6eb25a8255cfd513af59ca8e881ca289321c21f9
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-05-21 01:29:06 -0300

    feat: create Sidebar Drawer menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react"
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react"
+import { RiMenuLine } from "react-icons/ri"
+import { useSidebarDrawer } from "../../contexts/sidebarDrawerContext"
 import { Logo } from "./Logo"
 import { NotificationsNav } from "./NotificationsNav"
 import { Profile } from "./Profile"
 import { SearchBox } from "./SearchBox"
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -19,8 +23,20 @@ export function Header() {
       mx="auto"
       mt="4"
       align="center"
-      px="16"
+      px="6"
     >
+      {!isWideVersion && (
+        <IconButton
+          icon={<Icon as={RiMenuLine} />}
+          display="flex"
+          alignItems="center"
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          aria-label="Open Navigation"
+          mr="2"
+        ></IconButton>
+      )}
       <Logo />
 
       {isWideVersion && <SearchBox />}
